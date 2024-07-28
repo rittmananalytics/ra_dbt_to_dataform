2fb13d037b27acb2f2d1d8609c2a881779a5ad01
Mark Rittman <mark.rittman@rittmananalytics.com>
2024-07-28 23:55:27 +0100

    removed checkpoint files

--- ra_dbt_to_dataform 2/dbt_to_dataform/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# dbt_to_dataform/__init__.py
-
-from .repository_analyzer import RepositoryAnalyzer
-from .model_converter import ModelConverter
-from .metadata_converter import MetadataConverter
-from .project_generator import ProjectGenerator
-
-# You can also define a version for your package
-__version__ = "0.1.0"
